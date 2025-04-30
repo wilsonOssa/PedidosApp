68b23e36499e04cddacb9e49ecbf45550f5e94ee
wilsonOssa <wilson.ossa974@pascualbravo.edu.co>
2025-04-29 20:52:32 -0500

    feat: mostrar historial de pedidos

---
+++ PedidosApp/HistorialForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PedidosApp
+{
+    partial class HistorialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPedidos = new System.Windows.Forms.DataGridView();
+            this.colCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Metodo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Costo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPedidos
+            // 
+            this.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedidos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCliente,
+            this.Producto,
+            this.Metodo,
+            this.Costo});
+            this.dgvPedidos.Location = new System.Drawing.Point(12, 35);
+            this.dgvPedidos.Name = "dgvPedidos";
+            this.dgvPedidos.Size = new System.Drawing.Size(240, 150);
+            this.dgvPedidos.TabIndex = 0;
+            this.dgvPedidos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPedidos_CellContentClick);
+            // 
+            // colCliente
+            // 
+            this.colCliente.DataPropertyName = "Cliente";
+            this.colCliente.HeaderText = "Cliente";
+            this.colCliente.Name = "colCliente";
+            // 
+            // Producto
+            // 
+            this.Producto.DataPropertyName = "colProducto";
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // Metodo
+            // 
+            this.Metodo.DataPropertyName = "colMetodo";
+            this.Metodo.HeaderText = "Método de Entrega";
+            this.Metodo.Name = "Metodo";
+            // 
+            // Costo
+            // 
+            this.Costo.DataPropertyName = "colCosto";
+            this.Costo.HeaderText = "Costo";
+            this.Costo.Name = "Costo";
+            // 
+            // HistorialForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvPedidos);
+            this.Name = "HistorialForm";
+            this.Text = "HistorialForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPedidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Metodo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Costo;
+    }
+}
---
+++ PedidosApp/HistorialForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PedidosApp
+{
+    public partial class HistorialForm : Form
+    {
+        public HistorialForm()
+        {
+            InitializeComponent();
+            CargarPedidos();
+        }
+
+        private void CargarPedidos()
+        {
+            dgvPedidos.DataSource = RegistroPedidos.Instancia.Pedidos.Select(p => new
+            {
+                p.Cliente,
+                p.Producto,
+                Metodo = p.MetodoEntrega.TipoEntrega(),
+                Costo = p.ObtenerCosto()
+            }).ToList();
+        }
+    }
+}
